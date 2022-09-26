a4ced651a5945ff4dccb455b1c182765cef76158
sglkc <31957516+sglkc@users.noreply.github.com>
2022-09-26 10:02:36 +0700

    fix models/index.js
    add database port to .env

--- db/config.js
+++ db/config.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-  "username": process.env.DB_USER,
+  "username": process.env.DB_USER || 'root',
   "password": process.env.DB_PASSWORD,
-  "database": process.env.DB_NAME,
-  "host": process.env.DB_HOST,
-  "dialect": process.env.DB_DIALECT || 'mysql'
+  "database": process.env.DB_NAME || 'jracademy',
+  "host": process.env.DB_HOST || 'localhost',
+  "port": process.env.DB_PORT || 3306,
+  "dialect": 'mysql'
 };
