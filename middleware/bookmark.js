78c81d0693cafd210cf725335f171adf56572cd5
sglkc <31957516+sglkc@users.noreply.github.com>
2022-10-26 21:28:18 +0700

    feat: add search query to /categories
    chore: clean up bookmark middleware

--- middleware/bookmark.js
+++ middleware/bookmark.js
@@ -1,15 +1,17 @@
 const { literal } = require('sequelize');
 const includeBookmark = (user_id, col = 'Item.item_id') => (
   [
-    literal(`(
-     SELECT COUNT(bookmark.item_id)
-     FROM
-       bookmarks AS bookmark
-     WHERE
-       bookmark.user_id = ${user_id}
-       AND
-       bookmark.item_id = ${col}
-    )`),
+    literal(
+      `(
+       SELECT COUNT(bookmark.item_id)
+       FROM
+         bookmarks AS bookmark
+       WHERE
+         bookmark.user_id = ${user_id}
+         AND
+         bookmark.item_id = ${col}
+      )`.replace(/\s+/g, ' ')
+    ),
     'Bookmark'
   ]
 );
--- routes/categories.js
+++ routes/categories.js
@@ -1,14 +1,27 @@
+const { Op } = require('sequelize');
 const router = require('express').Router();
 const auth = require('#middleware/authentication');
 const { Category, Item } = require('#models');
 
+router.use(auth);
+
 router.get('/', (req, res) => {
-  Category.findAndCountAll({
-    group: ['name'],
-    attributes: ['name']
-  })
-    .then(({ count, rows }) => {
-      const result = rows.map((row) => row.name);
+  const { search } = req.query;
+  const searchArray = typeof(search) === 'object';
+
+  Category.findAll(
+    Object.assign({}, search && {
+      where: {
+        name: {
+          [searchArray ? Op.in : Op.like]: searchArray ? search : `%${search}%`
+        }
+      }
+    })
+  )
+    .then((rows) => {
+      const mapped = rows.map(row => row.name);
+      const result = [ ...new Set(mapped) ];
+      const count = result.length;
 
       return res.status(200).send({ result, count });
     })
@@ -17,6 +30,31 @@ router.get('/', (req, res) => {
       return res.status(500).send({ message: err });
     });
 });
+
+// TODO
+router.get('/items', (req, res) => {
+  const { page, limit } = req.query;
+  const perPage = parseInt(limit) || 5;
+  const offset = (parseInt(page) * perPage - perPage) || 0;
+
+  if (offset < 0) return res.status(400).send({ message: 'INVALID_PAGE' });
+
+  Category.findAndCountAll({
+    include: Item.unscoped(),
+    limit: perPage,
+    offset,
+  })
+    .then(({ count, rows }) => {
+      const result = rows;
+
+      return res.status(200).send({ result, count });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err });
+    });
+});
+
 
 router.get('/search', (req, res) => {
   const { q } = req.query;
