c589e3e2cc8a70c6c15f62b911602216c362fb1e
sglkc <31957516+sglkc@users.noreply.github.com>
2022-10-27 17:09:34 +0700

    feat: add /items to /categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,7 @@
 const { Op } = require('sequelize');
 const router = require('express').Router();
 const auth = require('#middleware/authentication');
+const includeBookmark = require('#middleware/bookmark');
 const { Category, Item } = require('#models');
 
 router.use(auth);
@@ -31,47 +32,34 @@ router.get('/', (req, res) => {
     });
 });
 
-// TODO
 router.get('/items', (req, res) => {
-  const { page, limit } = req.query;
+  const { page, limit, search } = req.query;
   const perPage = parseInt(limit) || 5;
   const offset = (parseInt(page) * perPage - perPage) || 0;
+  const { user_id } = res.locals;
 
   if (offset < 0) return res.status(400).send({ message: 'INVALID_PAGE' });
 
   Category.findAndCountAll({
-    include: Item.unscoped(),
+    attributes: {
+      include: [ includeBookmark(user_id, 'Category.item_id') ]
+    },
+    distinct: true,
+    group: ['item_id'],
+    include: Item,
     limit: perPage,
     offset,
+    where: {
+      name: { [Op.in]: search }
+    }
   })
     .then(({ count, rows }) => {
-      const result = rows;
+      const result = rows.map(row => ({
+        ...row.Item.toJSON(),
+        Bookmark: Boolean(row.get('Bookmark'))
+      }));
 
-      return res.status(200).send({ result, count });
-    })
-    .catch((err) => {
-      console.error(err);
-      return res.status(500).send({ message: err });
-    });
-});
-
-
-router.get('/search', (req, res) => {
-  const { q } = req.query;
-
-  if (!q) return res.status(400).send({ message: 'q query required' });
-
-  Category.findAll({
-    where: { name: q },
-    include: [
-      {
-        model: Item,
-        include: [ Category ]
-      }
-    ]
-  })
-    .then((result) => {
-      return res.status(200).send({ result });
+      return res.status(200).send({ result, count: count.length });
     })
     .catch((err) => {
       console.error(err);
