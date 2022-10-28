7fe68e461c20689977a377eb3d64e1bd42288460
sglkc <31957516+sglkc@users.noreply.github.com>
2022-10-28 19:56:43 +0700

    feat: add post /password for forgotten password
    build: add nodemailer

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 const auth = require('#middleware/authentication');
 const { User } = require('#models');
 
@@ -45,6 +47,57 @@ router.patch('/', async (req, res) => {
         return res.status(500).send({ message: err });
       });
   });
+});
+
+router.post('/password', async (req, res) => {
+  const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD } = process.env;
+  const user = await User.findByPk(user_id);
+
+  if (!req.body.token) return;
+
+  try {
+    const password = Math.random().toString().slice(2, 8);
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    await user.update({ password: hash });
+
+    const transporter = nodemailer.createTransport({
+      service: 'smtp',
+      host: SMTP_HOST,
+      port: SMTP_PORT,
+      secure: SMTP_PORT == 465,
+      auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false
+      },
+    });
+
+    await transporter.sendMail({
+      from: {
+        name: 'Yayasan Jalan Rahmat',
+        address: SMTP_USER
+      },
+      to: user.email,
+      subject: 'Setel Ulang Kata Sandi',
+      text: `Berikut adalah kata sandi baru untuk akun Anda:
+      ${password}
+      Jangan berikan kata sandi ini kepada siapapun!`,
+      html: `<div style="text-align: center">
+        <h4>Berikut adalah kata sandi baru untuk akun Anda:</h4>
+        <h3>${password}</h3>
+        <p>Jangan berikan kata sandi ini kepada siapapun!</p>
+      </div>`
+    });
+
+    return res.status(200).send({ message: 'EMAIL_SENT' })
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({ message: err });
+  }
 });
 
 router.patch('/password', async (req, res) => {
